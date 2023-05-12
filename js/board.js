805733fa15e74d1719a9de09f24f4a55da8764cd
Karl Massieux <kmassieux@localhost.localdomain>
2023-05-12 19:59:46 +0200

    Board class

--- js/board.js
+++ js/board.js
@@ -1,15 +1,73 @@
-export function drawSquare(canvas, ctx, [posX, posY], isColored = false) {
-    let squareSize = canvas.width / 8;
-    ctx.fillStyle = isColored ? "#593300" : "#ffc67a"
-    ctx.fillRect((posX-1) * squareSize,(posY-1) * squareSize, canvas.width / 8, canvas.height / 8);
-}
 
-export function drawBoard(canvas, ctx) {
-    for(let y = 1; y <= 8; y++)
-    {
-        for(let x = 1; x <= 8; x++)
+export class Board {
+
+    constructor(canvas, ctx) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+    }
+
+    drawSquare([posX, posY], isColored = false) {
+        let squareSize = this.canvas.width / 8;
+        this.ctx.fillStyle = isColored ? "#593300" : "#ffc67a"
+        this.ctx.fillRect((posX-1) * squareSize,(posY-1) * squareSize, this.canvas.width / 8, this.canvas.height / 8);
+    }
+    
+    drawBoard(canvas, ctx) {
+
+        for(let y = 1; y <= 8; y++)
         {
-            drawSquare(canvas, ctx, [x,y], ((x+y)%2 == 1));
+            for(let x = 1; x <= 8; x++)
+            {
+                this.drawSquare([x,y], ((x+y)%2 == 1));
+            }
+        }
+    }
+
+    screenToSquare(squareSize, [posX, posY]) {
+
+        let squareX = Math.floor(posX / squareSize) + 1;
+        let squareY = Math.floor(posY / squareSize) + 1;
+    
+        return [squareX, squareY];
+    }
+    
+    squareToScreen(squareSize, [squareX, squareY]) {
+
+        let posX = (squareX - 1) * squareSize;
+        let posY = (squareY - 1) * squareSize;
+    
+        return [posX, posY];
+    }
+    
+    hovering([squareX, squareY]) {
+        this.drawBoard();
+        this.ctx.fillStyle = "rgba(150, 200, 255, 0.5)";
+        let pos = this.squareToScreen(this.canvas.width / 8, [squareX,squareY]);
+        this.ctx.fillRect(pos[0], pos[1], this.canvas.width / 8, this.canvas.height / 8);
+    }
+
+    mouseMoveEvent(e) {
+        let posX = (e.clientX - this.canvas.getBoundingClientRect().x);
+        let posY = (e.clientY - this.canvas.getBoundingClientRect().y);
+    
+        //real square size
+        let squareSize = this.canvas.getBoundingClientRect().width / 8;
+    
+        let squarePos = this.screenToSquare(squareSize, [posX, posY]);
+    
+        let coordsText = "squareX: " + squarePos[0] + ", squareY: " + squarePos[1];
+        console.log("posX: " + posX + ", posY: " + posY);
+    
+        document.getElementById("position").innerHTML = coordsText;
+    
+        //If the mouse is over the board
+        if(posX >= this.canvas.getBoundingClientRect().x && posX <= this.canvas.getBoundingClientRect().right
+            && posY >= this.canvas.getBoundingClientRect().y && posY <= this.canvas.getBoundingClientRect().bottom)
+        {
+            this.hovering( this.screenToSquare(squareSize,[posX,posY]) );
+        } else {
+            console.log('outside');
+            this.drawBoard();
         }
     }
 }
